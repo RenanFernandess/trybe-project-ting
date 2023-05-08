75f2e7dff38418e00a705e6c1a86047c2888b269
Renan Fernandes <renzinestuods@gmail.com>
2023-05-08 14:48:20 -0300

    :sparkles: feat: Implementa a função txt_importer, que é capaz de importar notícias a partir de um arquivo TXT

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    """função é capaz de importar notícias a partir de um arquivo TXT"""
+    if not path_file[-4:] == ".txt":
+        return print("Formato inválido", file=sys.stderr)
+    try:
+        with open(path_file) as file:
+            return [line.removesuffix("\n") for line in file]
+    except FileNotFoundError:
+        return print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
