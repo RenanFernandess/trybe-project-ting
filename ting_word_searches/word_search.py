eebbc694bc72a8b581740ad82ae71d20f658c0ba
Renan Fernandes <renzinestuods@gmail.com>
2023-05-08 17:50:06 -0300

    :sparkles: feat: Implementa a função search_by_word, que busca uma palavra em todos os arquivos processados

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -2,7 +2,8 @@ from ting_file_management.queue import Queue
 
 
 def exists_word(word: str, instance: Queue):
-    """verifica a existência de uma palavra em todos os arquivos processados"""
+    """verifica a existência de uma palavra em todos os
+    arquivos processados."""
     result = list()
     for data in list(instance.data):
         occurrences = list()
@@ -23,4 +24,26 @@ def exists_word(word: str, instance: Queue):
 
 
 def search_by_word(word: str, instance: Queue):
-    """Aqui irá sua implementação"""
+    """Busca uma palavra em todos os arquivos processados."""
+    result = list()
+    for data in list(instance.data):
+        occurrences = list()
+        for index in range(len(data["linhas_do_arquivo"])):
+            if word.lower() in data["linhas_do_arquivo"][index].lower():
+                occurrences.append(
+                    {
+                        "linha": (index + 1),
+                        "conteudo": data["linhas_do_arquivo"][index],
+                    }
+                )
+
+        if len(occurrences):
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": data["nome_do_arquivo"],
+                    "ocorrencias": occurrences,
+                }
+            )
+
+    return result
