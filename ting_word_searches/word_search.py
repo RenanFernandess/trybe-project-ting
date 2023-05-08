dd139c83113aac47786ef0a3e8bd31a7c400d20c
Renan Fernandes <renzinestuods@gmail.com>
2023-05-08 17:40:31 -0300

    :sparkles: feat: Implementa a função exists_word, que verifica a existência de uma palavra em todos os arquivos processados

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,26 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
 
 
-def search_by_word(word, instance):
+def exists_word(word: str, instance: Queue):
+    """verifica a existência de uma palavra em todos os arquivos processados"""
+    result = list()
+    for data in list(instance.data):
+        occurrences = list()
+        for index in range(len(data["linhas_do_arquivo"])):
+            if word.lower() in data["linhas_do_arquivo"][index].lower():
+                occurrences.append({"linha": (index + 1)})
+
+        if len(occurrences):
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": data["nome_do_arquivo"],
+                    "ocorrencias": occurrences,
+                }
+            )
+
+    return result
+
+
+def search_by_word(word: str, instance: Queue):
     """Aqui irá sua implementação"""
