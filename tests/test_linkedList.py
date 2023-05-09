ef697d149bc674f2205b6e4083b8893810f38135
Renan Fernandes <renzinestuods@gmail.com>
2023-05-09 20:16:44 -0300

    :white_check_mark: test: test_if_it_is_possible_to_start_the_list_with_values

--- tests/test_linkedList.py
+++ tests/test_linkedList.py
@@ -17,3 +17,15 @@ def test_if_it_is_possible_to_add_value_to_the_view_and_access_them():
 
     li.add_first("op")
     assert li.get_first() == "op"
+
+
+def test_if_it_is_possible_to_start_the_list_with_values(values_list):
+    li = LinkedList(values_list)
+
+    assert li.get_first() == "x"
+    assert li.get_last() == "u"
+
+    li = LinkedList("test")
+
+    assert li.get_first() == "test"
+    assert li.get_last() == "test"
