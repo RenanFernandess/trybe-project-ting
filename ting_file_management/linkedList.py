ae32b7d67d6e897cbe6f8b349d2b75ce8a856319
Renan Fernandes <renzinestuods@gmail.com>
2023-05-10 11:46:08 -0300

    :sparkles: feat: Cria os metodos __iterate_left, __iterate_right e __iterate, que serão utilizados para retornar um valor especifico

--- ting_file_management/linkedList.py
+++ ting_file_management/linkedList.py
@@ -27,12 +27,43 @@ class LinkedList:
         raise StopIteration
 
     def __init_list(self, value):
+        """o(1)"""
         node = Node(value)
         self.__length = 1
         self.__head = node
         self.__tail = node
 
+    def __remove(self, node: Node):
+        """o(1)"""
+        node.prev.next = node.next
+        node.next.prev = node.prev
+        return node.value
+
+    def __iterate_left(self, stop: int, start=0):
+        """o(n)"""
+        node = self.__head.next
+        while start < stop:
+            start += 1
+            node = node.next
+        return node
+
+    def __iterate_right(self, stop: int, start=-1):
+        """o(n)"""
+        node = self.__tail.prev
+        while start > stop:
+            start -= 1
+            node = node.prev
+        return node
+
+    def __iterate(self, index: int):
+        """o(n)"""
+        if index < -1:
+            return self.__iterate_right(index, start=1)
+        return self.__iterate_left(index, start=-2)
+
     def extend(self, list):
+        """LinkedList = o(1)
+        other lista = o(n)"""
         if isinstance(list, LinkedList):
             self.__tail.next = list.__head
             list.__head.prev = self.__tail
@@ -42,6 +73,7 @@ class LinkedList:
                 self.add(value)
 
     def add(self, value):
+        """o(1)"""
         if self.__tail:
             node = Node(value, prev=self.__tail)
             self.__tail.next = node
@@ -51,6 +83,7 @@ class LinkedList:
             self.__init_list(value)
 
     def add_first(self, value):
+        """o(1)"""
         if self.__head:
             node = Node(value, next=self.__head)
             self.__head.prev = node
@@ -59,50 +92,34 @@ class LinkedList:
         else:
             self.__init_list(value)
 
-    def __remove(self, node: Node):
-        node.prev.next = node.next
-        node.next.prev = node.prev
-        return node.value
-
-    def pop(self, index=0):
-        if not (index > (0 - self.__length) and index < self.__length):
-            raise IndexError
-        if index == -1:
-            return self.pop_last()
-        if index == 0:
-            return self.pop_first()
-        return self.__pop_at(index)
-
-    def __pop_at(self, index: int):
-        node = None
-        if index > 0:
-            node = self.__head.next
-            curr_ind = 1
-            while curr_ind < index:
-                curr_ind += 1
-                node = node.next
-        if index < -1:
-            node = self.__tail.prev
-            curr_ind = -2
-            while curr_ind > index:
-                curr_ind -= 1
-                node = node.prev
-        return self.__remove(node)
-
     def pop_first(self):
+        """o(1)"""
         first = self.__head
         self.__head = self.__head.next
         self.__head.prev = None
         return first
 
     def pop_last(self):
+        """o(1)"""
         last = self.__tail
         self.__tail = self.__tail.prev
         self.__tail.next = None
         return last
 
+    def pop(self, index=0):
+        """o(n)"""
+        if not (index > (0 - self.__length) and index < self.__length):
+            raise IndexError
+        if index == -1:
+            return self.pop_last()
+        if index == 0:
+            return self.pop_first()
+        return self.__remove(self.__iterate(index))
+
     def get_first(self):
+        """o(1)"""
         return self.__head.value
 
     def get_last(self):
+        """o(1)"""
         return self.__tail.value
